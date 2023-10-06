61a2cb5851275bb775a4e9d0a88ca7c7dc654982
nabraga <nzbg@hotmail.com>
2023-10-06 08:25:45 -0300

    auth back done

--- backend/db/src/controller/UserController.js
+++ backend/db/src/controller/UserController.js
@@ -1,4 +1,5 @@
 // controllers/userController.js
+require('dotenv').config()
 
 const { where } = require('sequelize');
 const User = require('../models/User');
@@ -8,16 +9,16 @@ const jwt = require('jsonwebtoken')
 module.exports = {
     async createUser(req, res) {
         try {
-            const { name, email, password,confirmPassword, status } = await req.body;
+            const { name, email, password, confirmPassword, status } = await req.body;
             const user = await User.findOne({ where: { email: email } });
-            
-            if(name === '' || email === '' || password === '') {
+
+            if (name === '' || email === '' || password === '') {
                 res.status(400).json({ error: 'Campos obrigatorio' });
             }
             if (user) {
                 res.status(400).json({ error: 'Email já cadastrado.' });
             } else {
-                if(password === confirmPassword){
+                if (password === confirmPassword) {
                     const salt = await bcrypt.genSalt(12)
                     const passwordHash = await bcrypt.hash(password, salt)
 
@@ -25,17 +26,17 @@ module.exports = {
                         name,
                         email,
                         password: passwordHash,
-                        
+
                     });
-                    res.status(200).json({ user: {name, email, password: passwordHash},msg: "Cadastrado com sucesso" })                 
-                }else{                    
+                    res.status(200).json({ user: { name, email, password: passwordHash }, msg: "Cadastrado com sucesso" })
+                } else {
                     res.status(400).json({ error: 'senha nao confere' });
                 }
             }
 
-            
-            
-               
+
+
+
         } catch (error) {
             res.status(400).json({ error: 'Erro interno do servidor.' });
             //console.log({error})
@@ -43,43 +44,43 @@ module.exports = {
     },
     async loginUser(req, res) {
 
-        const{email, password} = req.body
+        const { email, password } = req.body
         const user = await User.findOne({ where: { email: email } });
-        if(email === '' || password === '') {
+        if (email === '' || password === '') {
             res.status(400).json({ error: 'Campos obrigatorio' });
         }
-        if(!user){            
+        if (!user) {
             res.status(400).json({ error: 'usuario nao encontrado' });
         }
         const checkPassword = await bcrypt.compare(password, user.password)
-        
-        
-        if(!checkPassword){
+
+
+        if (!checkPassword) {
             res.status(400).json({ error: 'email ou senha incorretos' });
         }
 
         try {
             const secret = process.env.SECRET
-            const token = jwt.sign({id:user._id},secret, )
-            res.status(200).json({msg: "Autenticado com sucesso!"}, token)
+            const token = jwt.sign({ id: user._id }, secret,)
+            res.status(200).json({ msg: "Autenticado com sucesso!",token})
 
-            
+
         } catch (error) {
-            
+
         }
 
-    },    
+    },
     async updateUser(req, res) {
         try {
             const { id } = req.params
-            const { name, email,password, token, status } = await req.body;
-            const user = await User.findOne({ where: {id} })
+            const { name, email, password, token, status } = await req.body;
+            const user = await User.findOne({ where: { id } })
 
             if (!user) {
                 res.status(400).json({ error: 'nao encontrado' });
             } else {
-                const user = await User.update({ name, email,password, token, status }, { where: { id } })
-                res.status(200).json({ user: {name, email} })
+                const user = await User.update({ name, email, password, token, status }, { where: { id } })
+                res.status(200).json({ user: { name, email } })
                 return user
             }
 
@@ -91,43 +92,43 @@ module.exports = {
     async listUsers(req, res, next) {
         try {
             const users = await User.findAll()
-            
-            if(!users){
-                res.status(200).json({message: "nenhum resultado encontrado"})                
-            }else{
-                
+
+            if (!users) {
+                res.status(200).json({ message: "nenhum resultado encontrado" })
+            } else {
+
                 //const users = User.findAll()
-                res.status(200).json({users})
+                res.status(200).json({ users })
                 return users
             }
         } catch (error) {
-            
+
         }
     },
     async deleteUser(req, res) {
         const { id } = req.params
-        const user = await User.findOne({where: {id}})
-        
-        if(!user){
-            res.status(200).json({ message: "nenhum resultado encontrado"})
-        }else {
+        const user = await User.findOne({ where: { id } })
 
-            const user = await User.destroy({where: {id}})
-            res.status(200).json({ ok: true})
+        if (!user) {
+            res.status(200).json({ message: "nenhum resultado encontrado" })
+        } else {
+
+            const user = await User.destroy({ where: { id } })
+            res.status(200).json({ ok: true })
 
         }
     },
     async getUser(req, res) {
         const { id } = req.params
-        const user = await User.findOne({where: {id}})
-        
-        if(!user){
-            res.status(400).json({ message: "nenhum resultado encontrado"})
-        }else {
+        const user = await User.findOne({ where: { id } })
 
-            const user = await User.findOne({where: {id}})
-            const {name,email} = user            
-            res.status(200).json({ user: {id, name, email} })  
+        if (!user) {
+            res.status(400).json({ message: "nenhum resultado encontrado" })
+        } else {
+
+            const user = await User.findOne({ where: { id } })
+            const { name, email } = user
+            res.status(200).json({ user: { name, email } })
             return user
         }
     }
--- backend/db/src/routes.js
+++ backend/db/src/routes.js
@@ -1,39 +1,43 @@
+require('dotenv').config()
 const {Router} = require('express')
 const UserController = require('./controller/UserController')
+const jwt = require('jsonwebtoken')
 
 
 const router = Router()
 
+// refaturar como importação futuramente
 function checkToken(req,res,next){
-    const authHeader = req.headers['autorization']
-    const token = authHeader && authHeader.split(" ")[1]
-
+    
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    
+    
     if(!token){
-       return res.status(401).json({msg:"acsso negado"})
+       return res.status(401).json({msg:"acesso negado"})
     }
-    try {
+    
+    try {        
         const secret = process.env.SECRET
         jwt.verify(token, secret)
         next()
         
     } catch (error) {
-        return res.status(401).json({msg:"acsso negado - token"})
+        return res.status(401).json({msg:"acesso negado - token"})
         
     }
 
-    
 }
-
 
 //open route
 router.post('/create-user', UserController.createUser )
 router.post('/login-user', UserController.loginUser )
-router.get('/get-user/:id',checkToken, UserController.getUser )
 
 //auth routes
-router.get('/list-users', UserController.listUsers )
-router.patch('/update-user/:id',  UserController.updateUser )
-router.delete('/delete-user/:id', UserController.deleteUser )
+router.get('/get-user/:id',checkToken, UserController.getUser )
+router.get('/list-users',checkToken, UserController.listUsers )
+router.patch('/update-user/:id',checkToken,  UserController.updateUser )
+router.delete('/delete-user/:id',checkToken, UserController.deleteUser )
 
 
 module.exports = router
